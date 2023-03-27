3d84de1e915ba95dc1b0bab2f68456c944cc3a42
61800399ctc <61800399@student.muskegonisd.org>
2023-03-27 14:14:00 -0400

    Add project files.

---
+++ 2048.cs
@@ -0,0 +1,111 @@
+﻿using Console_2048;
+using System;
+
+public class Twenty48
+{
+    #region Fields
+    private string[,] _board = new string[4,4]
+	{
+		{ " ", " ", " ", " " },
+		{ " ", " ", " ", " " },
+		{ " ", " ", " ", " " },
+		{ " ", " ", " ", " " },
+	};
+    #endregion
+    #region Properties
+    public string[,] Board 
+	{ 
+		get { return _board; } 
+		set { _board = value; } 
+	}
+    #endregion
+	private void Print() // Test code please remove later!
+	{
+		int count = 0;
+		foreach (var Square in _board)
+		{
+			
+			if (count % 4 == 0)
+			{
+				Console.WriteLine();
+			}
+            count++;
+            Console.Write(Square + ", ");
+		}
+		Console.WriteLine("Done!");
+	}
+	/// <summary>
+	/// Creates an new block in a random null square
+	/// 10% chance of producing a 4, otherwise always produces a value of 2
+	/// </summary>
+	private void New_Block()
+	{
+		int x;
+		int y;
+		int FourChance;
+        while (true)
+		{
+            Random Rnd = new Random();
+            x = Rnd.Next(4);
+            y = Rnd.Next(4);
+			FourChance = Rnd.Next(0, 11);
+            if (Board[x, y] == " ")
+            {
+				if (FourChance <= 1)
+				{
+					Board[x, y] = "4";
+					break;
+				}
+				else
+				{
+
+				}
+				Board[x, y] = "2";
+                break;
+            }
+        }
+	}
+	
+	private void Shift_Blocks(int Key)
+	{
+        #region Shift Right
+        if (Key == 100) // Char D, Shifts all blocks to the Right
+		{
+			for (int Quad_Check = 0;  Quad_Check < 4; Quad_Check++)
+			{
+				for (int Y = 0; Y < 4; Y++)
+				{
+                    for (int Square = 2; Square >= 0; Square--)
+                    {
+                        if (_board[Y, Square + 1] == " " && _board[Y, Square] != " ")
+                        {
+                            _board[Y, Square + 1] = _board[Y, Square];
+                            _board[Y, Square] = " ";
+                            Print();
+                        }
+                    }
+                }
+                
+            }
+		}
+        #endregion
+        if (Key == 97) // Char A
+		{
+
+		}
+	}
+	/// <summary>
+	/// Occurs when the player moves the blocks 
+	/// Checks for WASD
+	/// </summary>
+	/// <param name="Key">ASCII value of the first key entered</param>
+	public void Move(int Key)
+	{
+		if (Key == 100 || Key == 97 || Key == 115 || Key == 119) // only accepting D, A, S, W, (in that order)
+		{
+			Shift_Blocks(Key);
+			New_Block();
+		}
+		
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Console_2048
+{
+    internal class Program
+    {
+        private static Twenty48 Game = new Twenty48();
+        static void Main(string[] args)
+        {
+            Print(Game.Board);
+            while (true)
+            {
+                New_Turn();
+            }
+            
+        }
+        private static void Print(string[,] Board)
+        {
+            string output = null;
+            short count = 0;
+            foreach (var x in Board)
+            {
+                output += x + ", ";
+                
+                count++;
+                if (count % 4 == 0)
+                {
+                    output += "\n";
+                }
+            }
+            Console.WriteLine("\n" + output);
+        }
+        private static void New_Turn()
+        {
+            int x = Console.Read();
+            while (true)
+            {
+                if (x >= 97) // Eliminates reading of invalid value types, Valid type conditional
+                {
+                    Console.Clear();
+                    Game.Move(x);
+                    Console.WriteLine(x);
+                    Print(Game.Board);
+                    break;
+                }
+                else // invalid type condidtional, does not progress the game
+                {
+                    Console.Clear();
+                    Print(Game.Board);
+                    return;
+                }
+            }
+            
+            
+        }
+    
+    }
+}
